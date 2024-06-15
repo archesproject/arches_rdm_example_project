f6f3c8062315d8f7f6a375382765a8a3c42a939c
Christopher Byrd <cbyrd@fargeo.com>
2024-06-15 15:43:35 -0700

    nit

--- __init__.py
+++ __init__.py
@@ -1,2 +1,9 @@
-VERSION = (7, 6, 0, "alpha", 1)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
+VERSION = (
+    7,
+    6,
+    0,
+    "alpha",
+    1,
+)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
 FOO = (7, 6, 0, "alpha", 1)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
+FOOBAR = (7, 6, 0, "alpha", 1)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
